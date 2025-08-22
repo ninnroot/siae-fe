79409d21c85e8246dd1fe596d229a3a86989febc
MinLu-prog <mainlu897@gmail.com>
2025-08-23 00:05:32 +0630

    Added ourteam page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Topbar } from "@/components/topbar";
 import { Footer } from "@/components/footer";
+import OurTeam from "@/components/ourteam";
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen">
       <Topbar />
-      <p>home</p>
+      <OurTeam />
       <Footer />
     </div>
   );
